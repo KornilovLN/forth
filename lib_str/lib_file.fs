4471c9f7eea5b96d24db3e24a06213e3fefe2418
KornilovLN <ln.KornilovStar@gmail.com>
2025-03-02 03:54:52 +0200

    Edit lib_file.fs

---
+++ lib_str/lib_file.fs
@@ -0,0 +1,81 @@
+\ Пример чтения файла целиком в память
+\ Путь к файлу передается в качестве аргумента
+\ Выводится содержимое файла
+
+VARIABLE file-handle
+VARIABLE file-len
+VARIABLE buf-addr
+
+: get-file-size ( c-addr u -- n )
+  \ Открываем файл
+  r/o open-file
+  IF
+    ." Не удалось открыть файл" cr
+    drop 0 exit \ Возвращаем 0 в случае ошибки
+  THEN
+  
+  \ Сохраняем дескриптор файла
+  file-handle !
+  
+  \ Получаем размер файла
+  file-handle @ file-size throw d>s
+  
+  \ Запоминаем размер для возврата
+  dup
+  
+  \ Закрываем файл
+  file-handle @ close-file throw
+
+  swap drop
+  \ На стеке оставляется размер файла
+;
+
+\ Простое и надежное чтение файла в память и вывод его содержимого
+: read-file-content ( c-addr u -- )
+  \ Открываем файл
+  r/o open-file
+  IF
+    ." Не удалось открыть файл" cr
+    drop 0 exit \ Возвращаем 0 в случае ошибки
+  THEN
+  
+  \ Сохраняем дескриптор файла
+  file-handle !
+  
+  \ Получаем размер файла
+  file-handle @ file-size throw d>s
+
+  \ Сохраняем размер
+  dup file-len !
+
+  \ --- Собственно чиение файла ---
+
+  \ выделяем память для буфера
+  allocate throw buf-addr !
+  
+  \ Читаем файл в буфер
+  buf-addr @ file-len @ file-handle @ read-file throw drop
+  
+  \ Закрываем файл
+  file-handle @ close-file throw
+  
+  \ Выводим содержимое
+  buf-addr @ file-len @ type
+  
+  \ Освобождаем память
+  buf-addr @ free throw
+;
+
+
+\ Проверяем существует ли файл перед чтением
+: cat ( c-addr u -- )
+  2dup r/o open-file
+  IF
+    drop
+    ." Файл '" type ." ' не найден в текущей директории!" cr
+  ELSE
+    close-file throw
+    read-file-content
+  THEN
+;
+
---
+++ lib_str/test_libfile.fs
@@ -0,0 +1,24 @@
+include lib_file.fs
+
+\ Слово для чтения файла в память и вывода его содержимого в консоль
+\ : cat ( c-addr u -- n ) ... ; 
+\ Использование:    s"filename.txt" cat cr
+.S cr
+s" ../tut/read_file.fs" cat
+cr cr
+.S cr
+
+
+\ Вывод размера файла без чтения файла в память:
+\ : get-file-size ( c-addr u -- ) ... ;
+\ Использование:    s"filename.txt" get-file-size
+s" ../tut/read_file.fs" get-file-size           \ в стеке размер файла       
+.S cr
+." Размер файла ../tut/read_file.fs = " . cr    \ снять со стека и вывести точкой
+.S cr
+." получен словом get-file-size" cr
+.S cr
+cr ." Строка " s" tttеееФФФ" type cr
+.S cr
+
+bye
